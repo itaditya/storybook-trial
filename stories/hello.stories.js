399cefe157fbe7343c6fe17ca444a9b8fd67783f
Aditya Agarwal <adityaa803@gmail.com>
2018-07-02 11:24:51 +0530

    static storybook added

--- stories/hello.stories.js
+++ stories/hello.stories.js
@@ -4,6 +4,8 @@ import { action } from '@storybook/addon-actions';
 
 import Hello from '../Hello';
 
-storiesOf('Hello')
+const module = 'hmr'
+
+storiesOf('Hello', module)
   .add('default', () => <Hello message={'default'} onClick={action('hello')} />)
   .add('with message only', () => <Hello message={'hello'} />)
